abda9e529d4881f1bc883da687157d063b848bf1
Amruth Pillai <im.amruth@gmail.com>
2025-11-14 01:36:15 +0100

    force nitro

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,7 @@ const config = defineConfig({
 		tsconfigPaths(),
 		tailwindcss(),
 		tanstackStart({ router: { semicolons: true, quoteStyle: "double" } }),
-		process.env.NITRO ? nitro() : undefined,
+		nitro(),
 		viteReact({
 			babel: {
 				compact: false,
