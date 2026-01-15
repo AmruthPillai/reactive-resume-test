8691c6d5f269ba30c18b74a53c14e8b80ce37c15
Amruth Pillai <im.amruth@gmail.com>
2026-01-15 15:13:37 +0100

    add bg-background to loading screen

--- src/components/layout/loading-screen.tsx
+++ src/components/layout/loading-screen.tsx
@@ -3,7 +3,7 @@ import { Spinner } from "@/components/ui/spinner";
 
 export function LoadingScreen() {
 	return (
-		<div className="fixed inset-0 z-50 flex h-svh w-svw items-center justify-center gap-x-3">
+		<div className="fixed inset-0 z-50 flex h-svh w-svw items-center justify-center gap-x-3 bg-background">
 			<Spinner className="size-6" />
 			<p className="text-muted-foreground">
 				<Trans>Loading...</Trans>
